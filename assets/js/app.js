0007c37478b160e3df9fc31a1fd1efaddb008ac8
Kgor <kevin.gorham@gmail.com>
2020-04-17 14:17:31 -0700

    moved logic into functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,12 +5,15 @@ let userChoice;
 
 function getUserChoice() {
     // prompt the user to input R P or S
-    let userChoice = prompt("Choose R P or S");
+    userChoice = prompt("Choose R P or S");
     userChoice = userChoice.toLowerCase();
+    validateChoice();
 };
 
-
-
+function generateComputerChoice() {
+    const randomNum = Math.floor(Math.random() * 3);
+    compChoice = possibleChoices[randomNum];
+}
 
 function validateChoice() {
     // if they type something else
@@ -22,22 +25,20 @@ function validateChoice() {
     // once the user has made a choice
     else {
         // randomly generate a choice for the computer
-        const randomNum = Math.floor(Math.random() * 3)
-        compChoice = possibleChoices[randomNum]
+        generateComputerChoice();
+        compareAnswers();
+    }
+};
+
+function compareAnswers() {
+    // compare the two choices
+    if (userChoice === compChoice) {
+        alert('its a tie!')
+    } else if ((userChoice === "r" && compChoice === "s") || (userChoice === "p" && compChoice === "r") || (userChoice === "s" && compChoice === "p")) {
+        alert('You win!')
+    } else {
+        alert('You lose!')
     }
 }
 
-
-console.log('User choice: ' + userChoice)
-console.log('Comp choice: ' + compChoice)
-
-// compare the two choices
-if (userChoice === compChoice) {
-    alert('its a tie!')
-} else if ((userChoice === "r" && compChoice === "s") || (userChoice === "p" && compChoice === "r") || (userChoice === "s" && compChoice === "p")) {
-    alert('You win!')
-} else {
-    alert('You lose!')
-}
-// tell the user if the tied, or lost
 getUserChoice();
