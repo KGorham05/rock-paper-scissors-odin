b5a334f2388723334a7e79d56f6b04fc0fcdb5eb
Kgor <kevin.gorham@gmail.com>
2020-04-17 14:59:24 -0700

    added rps images to assets folder, scoreboard updating on win/loss/tie

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@
 const possibleChoices = ["r", "p", "s"];
 let compChoice;
 let userChoice;
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
 function getUserChoice() {
     // prompt the user to input R P or S
@@ -13,13 +16,13 @@ function getUserChoice() {
 function generateComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3);
     compChoice = possibleChoices[randomNum];
-}
+};
 
 function validateChoice() {
     // if they type something else
     if (userChoice !== "r" && userChoice !== "p" && userChoice !== "s") {
         // tell them to input R P or S
-        alert('You must choose "R", "P", or "S"')
+        alert('You must choose "R", "P", or "S"');
         getUserChoice();
     }
     // once the user has made a choice
@@ -27,18 +30,28 @@ function validateChoice() {
         // randomly generate a choice for the computer
         generateComputerChoice();
         compareAnswers();
-    }
+    };
 };
 
 function compareAnswers() {
     // compare the two choices
     if (userChoice === compChoice) {
         alert('its a tie!')
+        ties++;
     } else if ((userChoice === "r" && compChoice === "s") || (userChoice === "p" && compChoice === "r") || (userChoice === "s" && compChoice === "p")) {
         alert('You win!')
+        wins++;
     } else {
         alert('You lose!')
-    }
-}
+        losses++;
+    };
+    updateScoreboard();
+};
 
-getUserChoice();
+function updateScoreboard() {
+    $("#wins-display").text(wins);
+    $("#losses-display").text(losses);
+    $("#ties-display").text(ties);
+};
+
+$("#start-game").click(getUserChoice);
